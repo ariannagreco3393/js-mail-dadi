ebdfd165286967e8a098de25d22231edea5fc93c
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-29 16:06:20 +0200

    add dadi game

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 */
 
-//lista email
+/* //lista email
 const emailVerified = ['pippo@gmail.com', 'pluto@gmail.com', 'topolino@gmail.com'];
 
 //chiedi all'utente la sua email
@@ -16,7 +16,6 @@ for (let i = 0; i < emailVerified.length; i++) {
 
     if (userEmail == email) {
         vero_falso = true;
-
     } 
 }
 
@@ -25,3 +24,27 @@ if (vero_falso == true) {
 } else {
     console.log("mail errata");    
 }
+ */
+
+
+/* 
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia 
+per il computer. Stabilire il vincitore, in base a chi 
+fa il punteggio più alto.
+*/
+const giocatore = Math.floor(Math.random() * (7 - 1)+1);
+console.log(giocatore);
+const computer = Math.floor(Math.random() * (7 - 1)+1);
+console.log(computer);
+
+
+if (giocatore > computer) {
+    console.log("hai vinto");
+} else  if (giocatore == computer){
+    console.log("pari");
+} else {
+    console.log("hai perso");
+}
+
+
