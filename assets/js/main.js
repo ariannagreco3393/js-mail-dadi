87fc608471bdd446502388c95c1c4207b4b1f40c
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-29 15:38:39 +0200

    edit for cicle

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,19 @@ const emailVerified = ['pippo@gmail.com', 'pluto@gmail.com', 'topolino@gmail.com
 
 //chiedi all'utente la sua email
 const userEmail = prompt("inserisci la tua email");
+let vero_falso = false;
 
 for (let i = 0; i < emailVerified.length; i++) {
     const email = emailVerified[i];
-    
+
     if (userEmail == email) {
-        console.log("mail corretta");
-    } else {
-        console.log("mail errata");  
-    }
+        vero_falso = true;
+
+    } 
+}
+
+if (vero_falso == true) {
+    console.log("mail corretta");
+} else {
+    console.log("mail errata");    
 }
